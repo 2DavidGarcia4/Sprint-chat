308d4d34b78291561c2f22515e9041ee2c509f78
2DavidGarcia4 <gacub20ru@gmail.com>
2023-05-16 22:57:49 -0600

    add login and register

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,28 @@
+'use client'
+
+import styles from './login.module.scss'
+import { useUser } from '@/hooks/user'
+import CustomForm from '@/components/form/CustomForm'
+import CustomMessage from '@/components/message/CustomMessage'
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+
 export default function Login(){
+  const { isLoged } = useUser()
+  const router = useRouter()
+
+  useEffect(()=> {
+    setTimeout(()=> {
+      router.push('/')
+    }, 10000)
+  })
+
   return (
-    <section>
-      Login
+    <section className={styles.login}>
+      {isLoged ?
+        <CustomMessage content='Ya has iniciado sesión.' time={10} /> :
+        <CustomForm type='login' />
+      }
     </section>
   )
 }
