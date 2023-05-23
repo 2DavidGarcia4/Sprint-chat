25a9077865b3a7586463dd37eef1c11cea98bd52
2DavidGarcia4 <gacub20ru@gmail.com>
2023-05-23 13:50:14 -0600

    update login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,24 +4,29 @@ import styles from './login.module.scss'
 import { useUser } from '@/hooks/user'
 import CustomForm from '@/components/form/CustomForm'
 import CustomMessage from '@/components/message/CustomMessage'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 
 export default function Login(){
   const { isLoged } = useUser()
   const router = useRouter()
+  const [autoRedirect, setAutoRedirect] = useState(true)
 
   useEffect(()=> {
     if(isLoged){
+      setAutoRedirect(true)
       setTimeout(()=> {
         router.push('/')
       }, 10000)
+      
+    }else{
+      setAutoRedirect(false)
     }
-  })
+  }, [isLoged])
 
   return (
     <section className={styles.login}>
-      {isLoged ?
+      {(autoRedirect && isLoged) ?
         <CustomMessage content='Ya has iniciado sesión.' time={10} /> :
         <CustomForm type='login' />
       }
