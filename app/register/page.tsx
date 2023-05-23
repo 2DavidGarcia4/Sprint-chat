5728de87c40fd65924c5e1b5d8175e4e57cf621a
2DavidGarcia4 <gacub20ru@gmail.com>
2023-05-23 13:52:48 -0600

    update register

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -5,24 +5,29 @@ import styles from './register.module.scss'
 import { useUser } from '@/hooks/user';
 import CustomForm from "@/components/form/CustomForm";
 import CustomMessage from '@/components/message/CustomMessage';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 
 export default function Register(){
   const { isLoged } = useUser()
   const router = useRouter()
+  const [autoRedirect, setAutoRedirect] = useState(true)
 
   useEffect(()=> {
     if(isLoged){
+      setAutoRedirect(true)  
       setTimeout(()=> {
         router.push('/')
       }, 10000)
+    
+    }else{
+      setAutoRedirect(false)  
     }
-  })
+  }, [isLoged])
   
   return (
     <section className={styles.register}>
-      {isLoged ?
+      {(autoRedirect && isLoged) ?
         <CustomMessage content='Ya te has registrado.' time={10} /> :
         <CustomForm type="register" />
       }
