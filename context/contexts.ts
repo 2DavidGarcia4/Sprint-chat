5731f48e57762d1eab588eefeaef00cd7e74aac5
2DavidGarcia4 <gacub20ru@gmail.com>
2023-05-25 16:37:24 -0600

    add me context

--- context/contexts.ts
+++ context/contexts.ts
@@ -1,10 +1,26 @@
 import { User } from "@/utils/types"
 import { createContext, useContext, Dispatch, SetStateAction } from 'react';
 
+//* User provider
 interface UserContext {
   user: User | undefined, setUser: Dispatch<SetStateAction<User | undefined>>
 }
 
 export const UserContext = createContext<UserContext | undefined>(undefined)
+export function useUserCtx() {
+  return useContext(UserContext) as UserContext
+}
 
-export const useUserProv = () => useContext(UserContext) as UserContext
+//* Me provider
+interface MeContext {
+  userId?: string
+  valid?: boolean
+  setValid: Dispatch<SetStateAction<boolean | undefined>>
+  showValidator: boolean
+  setShowValidator: Dispatch<SetStateAction<boolean>>
+}
+
+export const MeContext = createContext<MeContext | undefined>(undefined)
+export function useMeCtx() {
+  return useContext(MeContext) as MeContext
+}
