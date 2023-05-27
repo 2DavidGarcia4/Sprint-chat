900868169b111a3801cd1720f7c807fd20cb16e3
2DavidGarcia4 <gacub20ru@gmail.com>
2023-05-26 22:17:32 -0600

    update user context

--- context/userProvider.tsx
+++ context/userProvider.tsx
@@ -1,11 +1,19 @@
 'use client'
 
 import { User } from "@/utils/types"
-import React, { useState } from "react"
+import { useState, useEffect, ReactNode } from "react"
 import { UserContext } from "./contexts"
+import { customFetch } from "@/utils/functions"
 
-export default function UserProvider({ children }: { children: React.ReactNode }){
+export default function UserProvider({ children }: { children: ReactNode }){
   const [user, setUser] = useState<User | undefined>(undefined)
+
+  useEffect(()=> {
+    customFetch(`users/@me`).then(res=> {
+      if(res.id) setUser(res)
+    })
+    .catch(()=> '')
+  }, [])
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
