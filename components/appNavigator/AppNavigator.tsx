28b76fc4223f36e4362f0809fb3ff264152fe47b
2DavidGarcia4 <gacub20ru@gmail.com>
2023-06-01 18:35:48 -0600

    update app navigator

--- components/appNavigator/AppNavigator.tsx
+++ components/appNavigator/AppNavigator.tsx
@@ -10,13 +10,15 @@ import { HiOutlineUser, HiUser } from 'react-icons/hi'
 import { BsGear, BsGearFill } from 'react-icons/bs'
 import { useUser } from '@/hooks/user'
 import { useUserCtx } from '@/context/contexts'
-import { MouseEvent } from 'react'
+import { MouseEvent, useEffect } from 'react'
 import CircleStatus from '../status/CircleStatus'
+import { useTooltip } from '@/hooks/useTooltip'
 
 export default function AppNavigator(){
   const pathname = usePathname()
   const { protectedRoute } = useUser()
-  const { user, setUser } = useUserCtx()
+  const { user } = useUserCtx()
+  const { addTooltip, events } = useTooltip()
 
   const isActiveRoute = (routName: string) => pathname.includes(routName)
 
@@ -32,12 +34,12 @@ export default function AppNavigator(){
   return (
     <nav className={`${styles.navigator} `}>
       <ul className={styles.navigator_elements}>
-        <li onClick={handlerClick} className={`${styles.navigator_elements_item} ${isActiveRoute('/chats') ? styles.selected : ''}`}>
+        <li {...events} onClick={handlerClick} className={`${styles.navigator_elements_item} ${isActiveRoute('/chats') ? styles.selected : ''}`} data-tooltip='Chats genial' data-direction='top'>
           <Link href={'/chats'}>
             {isActiveRoute('/chats') ? <BsChatLeftTextFill className={styles.navigator_icon}  /> : <BsChatLeftText className={styles.navigator_icon} />}
           </Link>
         </li>
-        <li onClick={handlerClick} className={`${styles.navigator_elements_item} ${isActiveRoute('/me') ? styles.selected : ''}`}>
+        <li {...events} onClick={handlerClick} className={`${styles.navigator_elements_item} ${isActiveRoute('/me') ? styles.selected : ''}`} data-tooltip='Tu perfil' data-direction='top'>
           <Link href={'/me'}>
             <div className={styles.navigator_elements_user}>
               {
@@ -52,12 +54,12 @@ export default function AppNavigator(){
             </div>
           </Link>
         </li>
-        <li onClick={handlerClick} className={`${styles.navigator_elements_item} ${isActiveRoute('/friends') ? styles.selected : ''}`}>
+        <li {...events} onClick={handlerClick} className={`${styles.navigator_elements_item} ${isActiveRoute('/friends') ? styles.selected : ''}`} data-direction='top' data-tooltip='Tus amigos'>
           <Link href={'/friends'}>
             {isActiveRoute('/friends') ? <RiContactsFill className={styles.navigator_icon} /> : <RiContactsLine className={styles.navigator_icon} />}
           </Link>
         </li>
-        <li onClick={handlerClick} className={`${styles.navigator_elements_item} ${isActiveRoute('/settings') ? styles.selected : ''}`}>
+        <li {...events} onClick={handlerClick} className={`${styles.navigator_elements_item} ${isActiveRoute('/settings') ? styles.selected : ''}`} data-direction='top' data-tooltip='Configuración'>
           <Link href={'/settings'}>
             {isActiveRoute('/settings') ? <BsGearFill className={styles.navigator_icon} /> : <BsGear className={styles.navigator_icon} />}
           </Link>
